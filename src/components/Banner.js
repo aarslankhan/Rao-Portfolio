fbb406092bd5f29def4dc8c903c460767494c432
aarslankhan <aarslanrashidofficial@gmail.com>
2024-09-16 18:11:40 +0500

    Intial Project Upload

---
+++ src/components/Banner.js
@@ -0,0 +1,77 @@
+import { useState, useEffect, useCallback } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.png";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Digital Transformation Specialist", "Seo Expert" ];
+  /*const period = 2000;*/
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+  
+    return () => { clearInterval(ticker) };
+  }, [text, delta, isDeleting])
+
+  const tick = useCallback(() => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  
+    if (isDeleting) {
+      setDelta(200); // fixed animation speed
+    } else {
+      setDelta(300); // fixed animation speed
+    }
+  
+    setText(updatedText);
+  
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  })
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Arslan`} <span className="txt-rotate" dataPeriod="500" data-rotate='[ "Web Developer, "Digital Transformation Specialist", "SEO Expert" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>With 4+ years of expertise in web development, I specialize in crafting responsive, scalable websites using the MERN stack (MongoDB, Express.js, React.js, Node.js). My focus is on delivering high-quality, user-friendly applications that align with modern industry standards and exceed client expectations.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
